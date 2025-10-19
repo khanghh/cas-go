f95011f9125d0be8384edf29e621a2059159644f
Khang Hoang <hoanghongkhang@gmail.com>
2025-10-19 16:32:15 +0700

    feat: refine system params

--- params/params.go
+++ params/params.go
@@ -13,16 +13,14 @@ const (
 	UserStateKeyPrefix            = "u:"
 	PendingUserExpiration         = 1 * time.Hour
 	ServiceTicketExpiration       = 1 * time.Minute
-	AuthStateTimeout              = 10 * time.Minute
-	TwoFactorChallengeMaxAttempts = 5                  // maximum number of verification attempts allowed per challenge; reset with user state
-	TwoFactorMaxFailCount         = 15                 // maximum total failed verification attempts per user; resets on successful verification
-	TwoFactorMaxOTPRequests       = 20                 // maximum number of OTP code requests allowed per user; reset with user state
-	TwoFactorMaxChallenges        = 5                  // maximum number of pending challenges allowed per user; reset with user state
-	TwoFactorStateMaxAge          = 24 * time.Hour     // time to live for user state
-	TwoFactorChallengeCooldown    = 5 * time.Minute    // cooldown duration for the challenge counter to reset
-	TwoFactorOTPExpiration        = 5 * time.Minute    // otp code expiration duration
-	TwoFactorOTPRefreshCooldown   = 1 * time.Minute    // otp code refresh cooldown
-	TwoFactorJWTExpiration        = 1 * time.Hour      // jwt token expiration duration
-	TrustedDeviceExpiration       = 7 * 24 * time.Hour // trusted device expiration duration
-	CSRFTokenExpiration           = 5 * time.Minute    // csrf token expiration duration
+	TwoFactorChallengeMaxAttempts = 5               // maximum number of verification attempts allowed per challenge; reset with user state
+	TwoFactorMaxFailCount         = 15              // maximum total failed verification attempts per user; resets on successful verification
+	TwoFactorMaxOTPRequests       = 20              // maximum number of OTP code requests allowed per user; reset with user state
+	TwoFactorMaxChallenges        = 10              // maximum number of pending challenges allowed per user; reset with user state
+	TwoFactorChallengeCooldown    = 5 * time.Minute // cooldown duration for the challenge counter to reset
+	TwoFactorOTPExpiration        = 5 * time.Minute // otp code expiration duration
+	TwoFactorOTPRefreshCooldown   = 1 * time.Minute // otp code refresh cooldown
+	TwoFactorStateMaxAge          = 24 * time.Hour  // time to live for user state
+	TwoFactorJWTExpiration        = 1 * time.Hour   // jwt token expiration duration
+	CSRFTokenExpiration           = 5 * time.Minute // csrf token expiration duration
 )
